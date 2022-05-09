e8eae9de18d29da5605a5d13df2d8c63c3c58332
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-05-09 13:32:18 +0530

    feat: add failsafe burn + lock in period

--- contracts/Mercury.sol
+++ contracts/Mercury.sol
@@ -6,50 +6,85 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Mercury is ERC20 {
-
     IERC721 NFTContract;
-    uint tokenID;
-    IERC20 token;
+    uint256 public tokenID;
+    IERC20 public token;
     address admin;
-    uint goal;
-    uint interest;
+    uint256 public goal;
+    uint256 interest;
     bool isPaid = false;
-    uint maxSupply = 100000 ether;
+    uint256 maxSupply = 100000 ether;
+    uint256 public deadline;
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == admin);
         _;
     }
 
-    address[] lendersArray; 
-    mapping(address => uint) lendersMap;
-    mapping(address => uint) burnersMap;
+    address[] lendersArray;
+    mapping(address => uint256) lendersMap;
+    mapping(address => uint256) burnersMap;
 
-    constructor(address _admin, address tokenAddress, uint _goal, address NFTAddress, uint _tokenID, uint _interest, string memory name) ERC20(string(abi.encodePacked("l_", name)), "MLT") {
+    constructor(
+        address _admin,
+        address tokenAddress,
+        uint256 _goal,
+        address NFTAddress,
+        uint256 _tokenID,
+        uint256 _interest,
+        string memory name
+    ) ERC20(string(abi.encodePacked("l_", name)), "MLT") {
         admin = _admin;
         token = IERC20(tokenAddress);
         goal = _goal;
         NFTContract = IERC721(NFTAddress);
         tokenID = _tokenID;
         interest = _interest;
+        deadline = block.timestamp + 15 days;
     }
 
-    function addLender(uint amount) public {
+    function addLender(uint256 amount) public {
         require(token.balanceOf(address(this)) <= goal, "Goal Reached");
-        require(NFTContract.balanceOf(address(this)) > 0, "NFT not in contract yet");
-        require(token.balanceOf(msg.sender) >= amount, "Insufficient Funds for Lending");
-        require(token.allowance(msg.sender, address(this)) >= amount, "Contract allowance insufficient");
-        require(totalSupply() + (amount / goal) * maxSupply <= maxSupply, "Liquidity not enough");
-        token.transferFrom(msg.sender, admin, amount);
+        require(
+            NFTContract.balanceOf(address(this)) > 0,
+            "NFT not in contract yet"
+        );
+        require(
+            token.balanceOf(msg.sender) >= amount,
+            "Insufficient Funds for Lending"
+        );
+        require(
+            token.allowance(msg.sender, address(this)) >= amount,
+            "Contract allowance insufficient"
+        );
+        require(
+            totalSupply() + (amount / goal) * maxSupply <= maxSupply,
+            "Liquidity not enough"
+        );
+        token.transferFrom(msg.sender, address(this), amount);
         lendersMap[msg.sender] = amount;
         lendersArray.push(msg.sender);
         _mint(msg.sender, (amount / goal) * maxSupply);
     }
 
+    function adminWithdraw() public onlyOwner {
+        require(block.timestamp >= deadline, "Lock-in period not over");
+        token.transfer(admin, goal);
+    }
+
     function repay() public onlyOwner {
-        require(token.balanceOf(msg.sender) >= goal + (interest * goal) / 100, "Insufficient Funds with caller");
-        token.transferFrom(msg.sender, address(this), goal + (interest * goal) / 100);
-        require(token.balanceOf(msg.sender) >= goal + (interest * goal) / 100, "Insufficient Funds for withdrawal");
+        require(
+            token.balanceOf(msg.sender) >= goal + (interest * goal) / 100,
+            "Insufficient Funds with caller"
+        );
+        require(
+            token.transferFrom(
+                msg.sender,
+                address(this),
+                (goal + (interest * goal) / 100)
+            ),
+            "Token transfer unsuccessful"
+        );
         isPaid = true;
         NFTContract.safeTransferFrom(address(this), admin, tokenID);
     }
@@ -57,15 +92,18 @@ contract Mercury is ERC20 {
     function payout() public {
         require(isPaid, "Admin hasn't paid debt");
         require(balanceOf(msg.sender) > 0, "Payout should be > 0");
-        uint amount = (balanceOf(msg.sender) * goal) / 100;
+        uint256 amount = (balanceOf(msg.sender) * goal) / 100;
         _burn(msg.sender, amount);
         lendersMap[msg.sender] = 0;
         token.transfer(msg.sender, amount + ((amount * interest) / 100));
     }
 
     function failSafeBurn() public {
-        require(balanceOf(msg.sender) > 0, "Does not have receipt tokens");
-        uint balance = balanceOf(msg.sender);
+        require(
+            balanceOf(msg.sender) > 0,
+            "Caller does not have receipt tokens"
+        );
+        uint256 balance = balanceOf(msg.sender);
         _burn(msg.sender, balance);
         burnersMap[msg.sender] = balance;
     }
